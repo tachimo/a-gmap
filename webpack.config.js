fa1b7930d5b0840d3e28478d4683925ae22bba08
tachimo <shihon17@gmail.com>
2024-12-11 01:14:59 +0900

    Fix:webpack周りの修正

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = (env, argv) => {
       path: path.resolve(__dirname, 'build'),
       filename: 'js/bundle.js',
       clean: true,
+      publicPath: '/',
     },
     mode: argv.mode || 'development', // mode の設定
     devtool: isProduction ? 'source-map' : 'eval-source-map', // ソースマップの設定
